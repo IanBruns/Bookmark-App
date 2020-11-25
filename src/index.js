6e3f6e3157f17568fe5b701e2013f3e0f95d1b3c
Ian Bruns <ian@rbruns.com>
2020-11-25 08:47:19 -0700

    adding empty files to be used later

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import $ from 'jquery';
 
+import './index.css';
+
 function main() {
     console.log('DOM is loaded');
-
-    const startMsg = $('<p>Webpack is working!</p>');
-    $('#root').append(startMsg);
 }
 
 $(main);
